a96e61779675eb72caa6fd84830a9b259876d632
DNSPropagation <60152258+DNSPropagation@users.noreply.github.com>
2020-02-22 12:11:36 +0100

    a

--- src/fr/creeper/ElliozBungee/JoinFriendCommand.java
+++ src/fr/creeper/ElliozBungee/JoinFriendCommand.java
@@ -1,16 +1,29 @@
 package fr.creeper.ElliozBungee;
 
+import java.io.File;
+
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class JoinFriendCommand extends Command {
+	
+	Saver SaveFriends;
 	public JoinFriendCommand() {
 		super("friend","");
 	}
 
 	 @Override
-	  public void execute(CommandSender commandSender, String[] strings) {
-	      commandSender.sendMessage(new ComponentBuilder("Hello world!").create());
+	  public void execute(CommandSender sender, String[] strings) {
+	      if(strings.length <= 1) {
+	    	  if(sender instanceof ProxiedPlayer) {
+	    		  ProxiedPlayer p = (ProxiedPlayer) sender;
+	    		  SaveFriends = new Saver(new File("playerfriends.properties"));
+	    		 
+	    		  p.sendMessage(new ComponentBuilder("You are already connected to the Hub!").create());
+	    		  
+	    	  }
+	      }
 	  }
 }
