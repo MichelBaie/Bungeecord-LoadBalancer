dd519cf78b067dc9e5d051cd5b46a59642ba81bb
DNSPropagation <60152258+DNSPropagation@users.noreply.github.com>
2020-02-22 12:40:18 +0100

    Dev

--- src/fr/creeper/ElliozBungee/JoinFriendCommand.java
+++ src/fr/creeper/ElliozBungee/JoinFriendCommand.java
@@ -8,20 +8,27 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class JoinFriendCommand extends Command {
-	
+	ElliozBungee b;
 	Saver SaveFriends;
 	public JoinFriendCommand() {
 		super("friend","");
 	}
 
-	 @Override
+	 @SuppressWarnings("deprecation")
+	@Override
 	  public void execute(CommandSender sender, String[] strings) {
 	      if(strings.length <= 1) {
 	    	  if(sender instanceof ProxiedPlayer) {
-	    		  ProxiedPlayer p = (ProxiedPlayer) sender;
+	    		  if(b.getProxy().getPlayer(strings[0]) != null) {
+	    		  ProxiedPlayer senderP = (ProxiedPlayer) sender;
+	    		  ProxiedPlayer requestedP = b.getProxy().getPlayer(strings[0]);
 	    		  SaveFriends = new Saver(new File("playerfriends.properties"));
-	    		 
-	    		  p.sendMessage(new ComponentBuilder("You are already connected to the Hub!").create());
+	    		 //SaveFriends.set(sender.getName(), requestedP.getName());
+	    		  requestedP.sendMessage("[Ellioz Friends] §e" + senderP.getName() + "§r vous invite à être son ami. Vous avez 60 secondes pour l'accepter ou le refuser");
+	    		  requestedP.sendMessage("§a Accepter");
+	    		  requestedP.sendMessage("§c Refuser");
+	    		  senderP.sendMessage(new ComponentBuilder("You are already connected to the Hub!").create());
+	    		  }
 	    		  
 	    	  }
 	      }
