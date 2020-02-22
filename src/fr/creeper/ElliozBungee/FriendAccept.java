bb7f205670437daca0768ca02e386f66419fd427
DNSPropagation <60152258+DNSPropagation@users.noreply.github.com>
2020-02-22 15:46:27 +0100

    Dev

--- src/fr/creeper/ElliozBungee/FriendAccept.java
+++ src/fr/creeper/ElliozBungee/FriendAccept.java
@@ -1,19 +1,30 @@
 package fr.creeper.ElliozBungee;
 
 import net.md_5.bungee.api.CommandSender;
+
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class FriendAccept extends Command {
-
+	ElliozBungee p;
 	public FriendAccept() {
 		super("friendaccept");
 		// TODO Auto-generated constructor stub
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		if(args.length <= 1) {
-			
+			if(sender instanceof ProxiedPlayer) {
+				ProxiedPlayer requestedP = (ProxiedPlayer) sender;
+			if(p.friends.get(requestedP) != null) {
+				ProxiedPlayer senderP = p.friends.get(requestedP);
+				senderP.sendMessage("[Ellioz Friends] " + requestedP.getName() + " à accepté votre requête !");
+				senderP.sendMessage("[Ellioz Friends] Toi et " + requestedP.getName() + " êtes maitenant amis.");
+				senderP.sendMessage("[Ellioz Friends] " + requestedP.getName() + " à accepté votre requête !");
+			}
+			}
 		}
 
 	}
