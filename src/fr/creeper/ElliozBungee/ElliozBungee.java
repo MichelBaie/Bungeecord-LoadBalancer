9db7d6cf6629751717981b254193dab6955d8fb1
Tristan <39345534+MichelBaie@users.noreply.github.com>
2020-02-22 15:16:41 +0100

    ;,

--- src/fr/creeper/ElliozBungee/ElliozBungee.java
+++ src/fr/creeper/ElliozBungee/ElliozBungee.java
@@ -1,13 +1,7 @@
 package fr.creeper.ElliozBungee;
 
-<<<<<<< HEAD
-
-
-=======
 import java.util.HashMap;
-
 import net.md_5.bungee.api.connection.ProxiedPlayer;
->>>>>>> d13fdd07e68abc1609e26dbf0dd61f21e8485cc4
 import net.md_5.bungee.api.plugin.Plugin;
 public class ElliozBungee extends Plugin {
 	HashMap<ProxiedPlayer, ProxiedPlayer> friends = new HashMap<ProxiedPlayer, ProxiedPlayer>();
