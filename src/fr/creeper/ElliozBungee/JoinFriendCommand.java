360379f4b59344c90042fdc2ad680b91b5cc5c26
DNSPropagation <60152258+DNSPropagation@users.noreply.github.com>
2020-02-22 12:47:28 +0100

    Dev

--- src/fr/creeper/ElliozBungee/JoinFriendCommand.java
+++ src/fr/creeper/ElliozBungee/JoinFriendCommand.java
@@ -3,7 +3,8 @@ package fr.creeper.ElliozBungee;
 import java.io.File;
 
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -24,10 +25,33 @@ public class JoinFriendCommand extends Command {
 	    		  ProxiedPlayer requestedP = b.getProxy().getPlayer(strings[0]);
 	    		  SaveFriends = new Saver(new File("playerfriends.properties"));
 	    		 //SaveFriends.set(sender.getName(), requestedP.getName());
+	    		  
+	    		  
+	    		  
+	    		  
+	    		 
 	    		  requestedP.sendMessage("[Ellioz Friends] §e" + senderP.getName() + "§r vous invite à être son ami. Vous avez 60 secondes pour l'accepter ou le refuser");
-	    		  requestedP.sendMessage("§a Accepter");
-	    		  requestedP.sendMessage("§c Refuser");
-	    		  senderP.sendMessage(new ComponentBuilder("You are already connected to the Hub!").create());
+	    		  
+	    		  
+	    		  TextComponent accept = new TextComponent( "Accepter (/friendaccept)" );
+	    		  accept.setColor( net.md_5.bungee.api.ChatColor.GREEN );
+	    		  accept.setBold( true );
+	    		  accept.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "friendaccept"));
+	    		  
+	    		  
+	    		  
+	    		  TextComponent deny = new TextComponent( "Refuser (/friendeny)" );
+	    		  deny.setColor( net.md_5.bungee.api.ChatColor.GREEN );
+	    		  deny.setBold( true );
+	    		  deny.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "friendeny"));
+	    		  
+	    		 requestedP.sendMessage(accept);
+	    		 requestedP.sendMessage(deny);
+	    		 
+	    		 
+	    		 
+	    		 
+	    		 
 	    		  }
 	    		  
 	    	  }
