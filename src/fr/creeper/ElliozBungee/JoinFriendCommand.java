ecf02b83a688ae52f471533aa1bdfc727183cd40
Tristan <39345534+MichelBaie@users.noreply.github.com>
2020-02-23 10:56:52 +0100

    Espece de Ksos

--- src/fr/creeper/ElliozBungee/JoinFriendCommand.java
+++ src/fr/creeper/ElliozBungee/JoinFriendCommand.java
@@ -16,6 +16,18 @@ public class JoinFriendCommand extends Command {
 	Saver SaveFriends;
 	public static Saver SaveFriendDemandeur;
 	public static Saver SaveFriendReceveur;
+	public String playerdemandeurfriendslot1 = SaveFriendDemandeur.get("friendslot1");
+	public String playerdemandeurfriendslot2 = SaveFriendDemandeur.get("friendslot2");
+	public String playerdemandeurfriendslot3 = SaveFriendDemandeur.get("friendslot3");
+	public String playerdemandeurfriendslot4 = SaveFriendDemandeur.get("friendslot4");
+	public String playerdemandeurfriendslot5 = SaveFriendDemandeur.get("friendslot5");
+	public String playerdemandeurinvitewating = SaveFriendDemandeur.get("invitewaiting");
+	public String playerecoisfriendslot1 = SaveFriendReceveur.get("friendslot1");
+	public String playerecoisfriendslot2 = SaveFriendReceveur.get("friendslot2");
+	public String playerecoisfriendslot3 = SaveFriendReceveur.get("friendslot3");
+	public String playerecoisfriendslot4 = SaveFriendReceveur.get("friendslot4");
+	public String playerecoisfriendslot5 = SaveFriendReceveur.get("friendslot5");
+	public String playerecoisinvitewaiting = SaveFriendReceveur.get("invitewaiting");
 	public JoinFriendCommand() {
 		super("friend","");
 	}
@@ -23,12 +35,14 @@ public class JoinFriendCommand extends Command {
 	 @SuppressWarnings("deprecation")
 	@Override
 	  public void execute(CommandSender sender, String[] strings) {
+		  
 	    	  if(sender instanceof ProxiedPlayer) {
 	    		  String playerecoisonline = "null";
 	    		  CommandSender playerdemandeur = sender;
 	    		  String playerecois = strings[0];
 	    		  SaveFriendDemandeur = new Saver(new File(playerdemandeur + "-friends.properties"));
 	    		  SaveFriendReceveur = new Saver(new File(playerecois + "-friends.properties"));
+	    		  
 	    		  
 	    		  ProxiedPlayer player = ProxyServer.getInstance().getPlayer(playerecois);
 	    		  if (player == null)
@@ -39,12 +53,10 @@ public class JoinFriendCommand extends Command {
 	    			  System.out.println("Le joueur (" + playerecois + ") est en ligne !");
 	    			  playerecoisonline = "true";
 	    		  }
-	    		  String playerdemandeurfriendslot1 = SaveFriendDemandeur.get("friendslot1");
-	    		  String playerdemandeurfriendslot2 = SaveFriendDemandeur.get("friendslot2");
-	    		  String playerdemandeurfriendslot3 = SaveFriendDemandeur.get("friendslot3");
-	    		  String playerdemandeurfriendslot4 = SaveFriendDemandeur.get("friendslot4");
-	    		  String playerdemandeurfriendslot5 = SaveFriendDemandeur.get("friendslot5");
 	    		  
+	    		  //
+	    		  // On Initialise les Amis de l'Envoyeur
+	    		  //
 	    		  if(playerdemandeurfriendslot1 == null) {
 	    			 System.out.println("Slot 1 de " + playerdemandeur + " : Pas définit, mise en place.");
 	    			 SaveFriendDemandeur.set("friendslot1", "");
@@ -70,6 +82,42 @@ public class JoinFriendCommand extends Command {
 		    			 SaveFriendDemandeur.set("friendslot5", "");
 		    			 System.out.println("Slot 5 de " + playerdemandeur + " : Définit !.");
 		    		  }
+	    		  if(playerdemandeurinvitewating == null) {
+		    			 System.out.println("Le joueur " + playerdemandeur + " n'a pas d'invitation en attente, mise en place.");
+		    			 SaveFriendDemandeur.set("invitewaiting", "");
+		    		  }
+	    		  //
+	    		  // On initialise les Amis du receveur
+	    		  //
+	    		  if(playerecoisfriendslot1 == null) {
+		    			 System.out.println("Slot 1 de " + playerecois + " : Pas définit, mise en place.");
+		    			 SaveFriendReceveur.set("friendslot1", "");
+		    			 System.out.println("Slot 1 de " + playerecois + " : Définit !.");
+		    		  }
+		    		  if(playerecoisfriendslot2 == null) {
+			    			 System.out.println("Slot 2 de " + playerecois + " : Pas définit, mise en place.");
+			    			 SaveFriendReceveur.set("friendslot2", "");
+			    			 System.out.println("Slot 2 de " + playerecois + " : Définit !.");
+			    		  }
+		    		  if(playerecoisfriendslot3 == null) {
+			    			 System.out.println("Slot 3 de " + playerecois + " : Pas définit, mise en place.");
+			    			 SaveFriendReceveur.set("friendslot3", "");
+			    			 System.out.println("Slot 3 de " + playerecois + " : Définit !.");
+			    		  }
+		    		  if(playerecoisfriendslot4 == null) {
+			    			 System.out.println("Slot 4 de " + playerecois + " : Pas définit, mise en place.");
+			    			 SaveFriendReceveur.set("friendslot4", "");
+			    			 System.out.println("Slot 4 de " + playerecois + " : Définit !.");
+			    		  }
+		    		  if(playerecoisfriendslot5 == null) {
+			    			 System.out.println("Slot 5 de " + playerecois + " : Pas définit, mise en place.");
+			    			 SaveFriendReceveur.set("friendslot5", "");
+			    			 System.out.println("Slot 5 de " + playerecois + " : Définit !.");
+			    		  }
+		    		  if(playerecoisinvitewaiting == null) {
+			    			 System.out.println("Le joueur " + playerecois + " n'a pas d'invitation en attente, mise en place.");
+			    			 SaveFriendReceveur.set("invitewaiting", "");
+			    		  }
 	    		  
 	    		  
 	    		  
@@ -80,44 +128,33 @@ public class JoinFriendCommand extends Command {
 	    		  {
 	    		      ProxiedPlayer senderP = (ProxiedPlayer) sender;
 	    			  senderP.sendMessage("Le joueur " + playerecois + " n'est pas en ligne actuellement !");
+	    			  return;
 	    		  } else {
 	    			  System.out.println("Le joueur (" + playerecois + ") est en ligne !");
 	    		  }
 	    		  
 	    		  if(sender instanceof ProxiedPlayer) {
+	    			  
+	    			  ProxiedPlayer senderP = (ProxiedPlayer) sender;
+	    			  ProxiedPlayer requestedP = ProxyServer.getInstance().getPlayer(strings[0]);
 	    		  
-	    		  ProxiedPlayer senderP = (ProxiedPlayer) sender;
-                  ProxiedPlayer requestedP = ProxyServer.getInstance().getPlayer(strings[0]);
-
-	    		  
-	    		  SaveFriends = new Saver(new File("playerfriends.properties"));
-	    		 SaveFriends.set(sender.getName(), requestedP.getName());
+	    			  requestedP.sendMessage("[Ellioz Friends] §e" + senderP.getName() + "§r vous invite à être son ami. Vous avez 60 secondes pour l'accepter ou le refuser");
 	    		  
 	    		  
-	    		  
-	    		  
-	    		 
-	    		  //requestedP.sendMessage("[Ellioz Friends] §e" + senderP.getName() + "§r vous invite à être son ami. Vous avez 60 secondes pour l'accepter ou le refuser");
-	    		  
-	    		  
-	    		  //TextComponent accept = new TextComponent( "Accepter (/friendaccept)" );
-	    		  //accept.setColor( net.md_5.bungee.api.ChatColor.GREEN );
-	    		  //accept.setBold( true );
-	    		  //accept.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "friendaccept"));
+	    			  TextComponent accept = new TextComponent( "Accepter (/friendaccept)" );
+	    			  accept.setColor( net.md_5.bungee.api.ChatColor.GREEN );
+	    			  accept.setBold( true );
+	    			  accept.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/friendaccept"));
 	    		  
 	    		  
 	    		  
-	    		  //TextComponent deny = new TextComponent( "Refuser (/friendeny)" );
-	    		  //deny.setColor( net.md_5.bungee.api.ChatColor.GREEN );
-	    		  //deny.setBold( true );
-	    		  //deny.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "friendeny"));
+	    			  TextComponent deny = new TextComponent( "Refuser (/friendeny)" );
+	    			  deny.setColor( net.md_5.bungee.api.ChatColor.GREEN );
+	    			  deny.setBold( true );
+	    			  deny.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/friendeny"));
 	    		  
-	    		 //requestedP.sendMessage(accept);
-	    		 //requestedP.sendMessage(deny);
-	    		 
-	    		 
-	    		//b.friends.put(requestedP, senderP);
-	    		 
+	    			  requestedP.sendMessage(accept);
+	    			  requestedP.sendMessage(deny);
 	    		 
 	    		  }
 	    		  
